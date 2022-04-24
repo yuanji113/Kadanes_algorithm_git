970c8ab574cb513b30a9ac9a9c6f1bacf1fc81cd
yuanji113 <yuan.ji@tamu.edu>
2022-04-24 17:16:13 -0500

    Check input type for two functions Test input type in unit tests

--- kadanes_algorithm.py
+++ kadanes_algorithm.py
@@ -1,6 +1,10 @@
 print("This file will show how we implement Kadane's algorithm to find the max/min contiguous subarray sum of a list.")
 
 def find_max_con_sub_sum(nums):
+    # check the input type, if not list, show TypeError
+    if type(nums) is not list:
+        raise TypeError("Invalid input for the function")
+
     maxSum = float('-inf')
     currSum = 0
 
@@ -13,6 +17,10 @@ def find_max_con_sub_sum(nums):
     return maxSum
 
 def find_min_con_sub_sum(nums):
+    # check the input type, if not list, show TypeError
+    if type(nums) is not list:
+        raise TypeError("Invalid input for the function")
+
     minSum = float('inf')
     currSum = 0
 
--- test_kadanes_algorithm.py
+++ test_kadanes_algorithm.py
@@ -10,9 +10,14 @@ class Test(TestCase):
         self.test_arr_2 = [2, -5, 1, -4, 3, -2]
         self.test_arr_2_res = -8
 
+        # test for input type
+        self.test_arr_3 = "12356"
+
     def test_find_max_con_sub_sum(self):
         self.assertEqual(kadanes_algorithm.find_max_con_sub_sum(self.test_arr_1), self.test_arr_1_res)
+        self.assertRaises(TypeError, kadanes_algorithm.find_max_con_sub_sum, self.test_arr_3)
 
     def test_find_min_con_sub_sum(self):
         self.assertEqual(kadanes_algorithm.find_min_con_sub_sum(self.test_arr_2), self.test_arr_2_res)
+        self.assertRaises(TypeError, kadanes_algorithm.find_min_con_sub_sum, self.test_arr_3)
 
