a442fa16635d99db09f84698df34e087f884798f
yuanji113 <yuan.ji@tamu.edu>
2022-04-24 16:55:04 -0500

    function find_max_con_sub_sum added

--- kadanes_algorithm.py
+++ kadanes_algorithm.py
@@ -1,1 +1,16 @@
-print("This file will show how we implement Kadane's algorithm to find the max/min subarray sum of a list.")
+print("This file will show how we implement Kadane's algorithm to find the max/min contiguous subarray sum of a list.")
+
+def find_max_con_sub_sum(nums):
+    maxSum = float('-inf')
+    currSum = 0
+
+    for num in nums:
+        if currSum < 0:
+            currSum = 0
+        currSum += num
+        maxSum = max(maxSum, currSum)
+
+    return maxSum
+
+
+
